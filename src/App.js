1c56cbf5de7061d9d00a57b97c1dca26257063e2
Raul Veras <verasraul@gmail.com>
2022-06-12 15:29:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
+import ChatFeed from './components/ChatFeed';
+
 function App() {
   return (
     <div className="App">
-      Hello World
+      <ChatEngine
+      // Props to send to ChatEngine
+      // how much height you want the chat app to take up on your website
+        height="100vh"
+        projectID="21da8f0c-140b-4308-a032-9e8aefcf52bc"
+        userName="john"
+        userSecret="qwerty"
+        renderChatFeed = { (chatAppProps) => <ChatFeed {...chatAppProps } />}
+        onNewMessage = { () => new Audio(`https://chat-engine-assets.s3.amazonaws.com/click.mp3`) }
+      />
     </div>
   );
 }
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,14 @@
+
+
+function ChatFeed(props) {
+    
+    console.log(props);
+
+    return (
+        <div>
+            <h1> ChatFeed </h1>
+        </div>
+    )
+}
+
+export default ChatFeed;
